b7a1ab3996317714b222828c9b82675de9f23f91
ArraygenR <66153555+ArraygenR@users.noreply.github.com>
2020-10-26 22:37:35 +0530

    Update gbk2fasta.py

--- Genbank-to-Fasta/gbk2fasta.py
+++ Genbank-to-Fasta/gbk2fasta.py
@@ -1,52 +1,3 @@
-def split_records(path):
-    f = open(path)
-    records = []
-    all_data = f.read()
-    for data in all_data.split("//"):
-        if data.strip() != '':
-            records.append(data)
-    return records
-
-def get_header(record):
-    lines = record.split("\n")
-    version =''
-    defination =''
-    part = ''
-    for line in lines:
-        if line[:5].strip()== '':
-            part += line+"\n"
-        else:
-            if "DEFINITION" in part:
-                defination = part.replace("DEFINITION", "").strip()
-            elif "VERSION" in part:
-                version = part.replace("VERSION", "").strip()
-            part =line+"\n"
-    return ">"+version+" "+defination
-
-def get_sequence(record):
-    lines = record.split("\n")
-    seq = ''
-    part = ''
-    for line in lines:
-        if line[:5].strip()== '':
-            part += line+"\n"
-        else:
-            if "ORIGIN" in part:
-                seq = part.replace("ORIGIN","").strip()
-            part =line+"\n"
-    if "ORIGIN" in part:
-        seq = part.replace("ORIGIN","")
-    seq = "\n".join(list(filter(None, [s[9:].replace(" ","").strip() for s in seq.split("\n")])))
-    return seq
-records = split_records("ls_orchid.gbk")
-
-record = records[0]
-header = get_header(record)
-seq = get_sequence(record)
-print(header)
-print(seq)
-
-
 import textwrap , sys
 def split_records(path):
     # opening file in reading mode
@@ -100,7 +51,7 @@ if __name__ == "__main__":
             if len(sys.argv) > 2:
                 output_file_name =  sys.argv[2]+".fasta"
             else:
-                output_file_name = sys.argv[1].strip("'").strip('"').replace(".gb",".fasta")
+                output_file_name = "sequence.fasta"
             # open file in which we want to write o/p
             fw = open(output_file_name, "w")
             # calling split function to get records
@@ -115,4 +66,3 @@ if __name__ == "__main__":
             print("Provide a GenBank file to convert to FASTA.")
     else:
         print("<input_file> not found.  Check the file path and try again.")
-
